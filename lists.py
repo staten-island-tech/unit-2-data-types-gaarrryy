5801445446e796ce37b0bc22e19956c11883fb3e
gaarrryy <158204285+gaarrryy@users.noreply.github.com>
2024-02-09 12:28:47 -0500

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,7 +1,25 @@
 n = 0
-
+n2 = 0
 n = float(input('what is your number?'))
-if n%2 == 0:
-    print ('even')
-else:
-    print ('odd')
+n == int(n)
+n = float(input('what is your second number?'))
+n == int(n2)
+factors = []
+
+for i in range(1,int(n/2)):
+    if n%i == 0:
+      if i not in factors:
+         factors.append(i)
+    if n/i not in factors:
+            factors.append(int(n/i))
+
+for i in range(1,int(n2/2)):
+    if n2%i == 0:
+      if i not in factors:
+         factors.append(i)
+    if n2/i not in factors:
+            factors.append(int(n2/i))
+
+for i in factors:
+    print (i)
+
